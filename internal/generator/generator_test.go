d95a40f357557b70467fcbd9c286b5433c09cec0
Oliver Otchere <oliverotchere4@gmail.com>
2025-01-18 10:46:43 +0000

    added complete test

--- internal/generator/generator_test.go
+++ internal/generator/generator_test.go
@@ -1,7 +1,6 @@
 package generator_test
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -11,7 +10,42 @@ import (
 	"github.com/matryer/is"
 )
 
-func Test_Can_Open_Template(t *testing.T) {
+func TestConvertMomentToGoFormat(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		var (
+			is = is.NewRelaxed(t)
+
+			format1 = "dddd, DD MMMM YYYY"
+			format2 = "DD-MM-YYYY"
+			format3 = "DD/MM/YYYY"
+			format4 = "DD/MM/YYYY HH:mm"
+			date    = "2012-04-24T18:25:43.511Z"
+		)
+
+		s1 := ConvertMomentToGoFormat(format1)
+		s2 := ConvertMomentToGoFormat(format2)
+		s3 := ConvertMomentToGoFormat(format3)
+		s4 := ConvertMomentToGoFormat(format4)
+
+		p, err := time.Parse(time.RFC3339, date)
+
+		is.NoErr(err)
+
+		r := p.Format(s1)
+		is.Equal(r, "Tuesday, 24 April 2012")
+
+		r = p.Format(s2)
+		is.Equal(r, "24-04-2012")
+
+		r = p.Format(s3)
+		is.Equal(r, "24/04/2012")
+
+		r = p.Format(s4)
+		is.Equal(r, "24/04/2012 18:25")
+	})
+}
+
+func TestCanOpenTemplate(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		var (
 			is = is.NewRelaxed(t)
@@ -23,17 +57,16 @@ func Test_Can_Open_Template(t *testing.T) {
 		_, err := os.ReadFile(templatePath)
 
 		is.NoErr(err)
-
 	})
 }
 
-func Test_CompileTemplate(t *testing.T) {
+func TestCompileTemplate(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		var (
 			is   = is.NewRelaxed(t)
 			name = "invoice"
 
-			g = newGenerator()
+			g = newGenerator(t)
 
 			data = map[string]interface{}{
 				"name":            "John Doe",
@@ -47,45 +80,53 @@ func Test_CompileTemplate(t *testing.T) {
 		htmlContent, err := g.CompileTemplate(name, data)
 
 		is.NoErr(err)
-		t.Log(htmlContent)
-		fmt.Println("content: ", htmlContent)
 		is.True(len(htmlContent) > 1)
 
 	})
 }
 
-func Test_GeneratePDF(t *testing.T) {
+func TestGeneratePDF(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		var (
 			is           = is.NewRelaxed(t)
-			g            = newGenerator()
-			templateName = "invoice"
+			g            = newGenerator(t)
+			templateName = "request_form"
 			data         = map[string]interface{}{
-				"name":        "John Doe",
-				"date":        time.Now(),
-				"tailwindcss": "http://localhost:3000/css/styles.css",
-				"logo":        "http://localhost:3000/images/logo.png",
-				"items": []map[string]interface{}{
-					{"name": "Item 1", "quantity": 2, "price": 10.00, "total": 20.00},
-					{"name": "Item 2", "quantity": 1, "price": 15.00, "total": 15.00},
-				},
-				"total": 35.00,
+				"patientName":        "Oliver Otcher",
+				"sex":                "M",
+				"date":               "2012-04-23T18:25:43.511Z",
+				"age":                "21",
+				"phoneNumber":        "052394748393",
+				"address":            "Anywhere",
+				"requestingDoctor":   "Dr tesr",
+				"requestingFacility": "Test fac",
+				"examination":        "ECR",
+				"query":              "Location",
 			}
 		)
 
 		pdfUrl, err := g.GeneratePDF(GenerationParam{
-			TemplateName: templateName,
-			Data:         data,
+			TemplateName:  templateName,
+			Data:          data,
+			RemoveMargins: true,
 		})
 
 		is.NoErr(err)
-		t.Log(pdfUrl)
-		fmt.Println("content: ", pdfUrl)
 		is.True(len(pdfUrl) > 1)
 	})
 
 }
 
-func newGenerator() *ChromeDp {
-	return &ChromeDp{}
+func newGenerator(t *testing.T) *ChromeDp {
+	is := is.NewRelaxed(t)
+	c, err := Connect()
+	is.NoErr(err)
+	return c
 }
+
+// func TestMain(m *testing.M) {
+//     setup()
+//     code := m.Run()
+//     shutdown()
+//     os.Exit(code)
+// }
